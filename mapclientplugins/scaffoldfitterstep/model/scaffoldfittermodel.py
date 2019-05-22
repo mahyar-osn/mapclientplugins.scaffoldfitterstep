9e050048a8f7f75fbe1de1378ccf93e56a8c5044
mahyar-osn <m.osanlouy@auckland.ac.nz>
2019-05-22 18:08:27 +1200

    Added basic methods for the high level scaffoldfitter model.

--- mapclientplugins/scaffoldfitterstep/model/scaffoldfittermodel.py
+++ mapclientplugins/scaffoldfitterstep/model/scaffoldfittermodel.py
@@ -1,94 +1,48 @@
 import sys
 
 from opencmiss.zinc.context import Context
+from opencmiss.zinc.field import Field, FieldFindMeshLocation
 from opencmiss.zinc.glyph import Glyph
+from opencmiss.zinc.graphics import Graphics
 from opencmiss.zinc.material import Material
+from opencmiss.zinc.scenefilter import Scenefilter
+from opencmiss.zinc.scenecoordinatesystem import SCENECOORDINATESYSTEM_NORMALISED_WINDOW_FIT_LEFT
 from opencmiss.zinc.status import OK as ZINC_OK
 
-from scaffoldfitter.src.scaffoldfitter.utils.field_utils import createFiniteElementField, getModelCoordinateField, getMesh
+from .master import MasterModel
 
 
 class ScaffoldFitterModel(object):
     def __init__(self):
         self._context = Context('scaffoldfit')
-        materialmodule = self._context.getMaterialmodule()
-        materialmodule.beginChange()
-        materialmodule.defineStandardMaterials()
-        # self._shareable_widget = image_context_data.get_shareable_open_gl_widget()
-        self._surfaceMaterial = materialmodule.createMaterial()
-        self._surfaceMaterial.setName('surfacefit')
-        self._surfaceMaterial.setAttributeReal3(Material.ATTRIBUTE_AMBIENT, [0.7, 0.7, 1.0])
-        self._surfaceMaterial.setAttributeReal3(Material.ATTRIBUTE_DIFFUSE, [0.7, 0.7, 1.0])
-        self._surfaceMaterial.setAttributeReal3(Material.ATTRIBUTE_SPECULAR, [0.5, 0.5, 0.5])
-        self._surfaceMaterial.setAttributeReal(Material.ATTRIBUTE_ALPHA, 0.5)
-        self._surfaceMaterial.setAttributeReal(Material.ATTRIBUTE_SHININESS, 0.3)
-        materialmodule.endChange()
-        glyphmodule = self._context.getGlyphmodule()
-        glyphmodule.defineStandardGlyphs()
-        tessellationmodule = self._context.getTessellationmodule()
-        defaultTessellation = tessellationmodule.getDefaultTessellation()
-        defaultTessellation.setRefinementFactors([12])
 
+        self._master_model = MasterModel(self._context)
+        self._region = self._master_model.get_region()
+        self._material_module = self._master_model.get_material_module()
+
+    def _clear_all(self):
+        self._scaffold_model = None
+        self._point_cloud = None
         self._modelReferenceCoordinateField = None
 
     def get_context(self):
         return self._context
 
-    # def _get_shareable_open_gl_widget(self):
-    #     context = self._model.get_context()
-    #     self._shareable_widget = BaseSceneviewerWidget()
-    #     self._shareable_widget.set_context(context)
-    #
-    # def get_shareable_open_gl_widget(self):
-    #     return self._shareable_widget
-
-    def set_location(self, location):
-        self._location = location
-
     def get_region(self):
         return self._region
 
-    def get_scene(self):
-        return self._region.getScene()
+    def initialise(self, model, point_cloud):
+        self._load_scaffold_model(model)
+        self._load_point_cloud(point_cloud)
+        self._master_model.initialize_problem()
 
-    def set_zinc_model_file(self, zincModelFile):
-        self._zincModelFile = zincModelFile
+    def set_align_settings_change_callback(self, align_settings_chhange_callback):
+        self._master_model.set_align_settings_change_callback(align_settings_chhange_callback)
 
-    def setZincPointCloudFile(self, zincPointCloudFile):
-        self._zincPointCloudFile = zincPointCloudFile
+    def _load_scaffold_model(self, model):
+        self._scaffold_model = model
+        self._master_model.set_scaffold_model(self._scaffold_model)
 
-    def setPointCloudData(self, pointCloudData):
-        self._pointCloudData = pointCloudData
-
-    def initialise(self):
-        self._region = self._context.createRegion()
-        # self.setStateAlign()
-        self._load()
-
-    def _load(self):
-        if self._modelReferenceCoordinateField is None:
-            result = self._region.readFile(self._zincModelFile)
-            if result != ZINC_OK:
-                raise ValueError('Failed to read reference model')
-            self._modelReferenceCoordinateField = getModelCoordinateField(self._region, self._modelReferenceCoordinateField)
-        self._mesh = getMesh(self._region)
-        self._show_model_graphics()
-
-    def _show_model_graphics(self):
-        scene = self._region.getScene()
-        scene.beginChange()
-        scene.removeAllGraphics()
-        materialmodule = scene.getMaterialmodule()
-        axes = scene.createGraphicsPoints()
-        pointAttr = axes.getGraphicspointattributes()
-        pointAttr.setGlyphShapeType(Glyph.SHAPE_TYPE_AXES_XYZ)
-        lines = scene.createGraphicsLines()
-        if self._mesh.getDimension() == 3:
-            lines.setExterior(True)
-        lines.setName('fit-lines')
-        surfaces = scene.createGraphicsSurfaces()
-        surfaces.setName('fit-surfaces')
-        surfaces.setMaterial(self._surfaceMaterial)
-        scene.endChange()
-
-
+    def _load_point_cloud(self, point_cloud):
+        self._point_cloud = point_cloud
+        self._master_model.set_point_cloud(self._point_cloud)
