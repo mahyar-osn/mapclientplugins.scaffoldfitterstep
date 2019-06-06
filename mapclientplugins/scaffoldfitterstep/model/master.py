e6ea31a87b207b61e8cf95175e9956560be54cc9
mahyar-osn <m.osanlouy@auckland.ac.nz>
2019-06-06 20:58:23 +1200

    Methods to signal initialization of scaffold and data + swap Y-Z axes + auto align model on data.

--- mapclientplugins/scaffoldfitterstep/model/master.py
+++ mapclientplugins/scaffoldfitterstep/model/master.py
@@ -7,8 +7,7 @@ class MasterModel(object):
 
     def __init__(self, context):
 
-        # self._context = Context(context)
-        self._context = context
+        self._context = Context(context)
         self._scaffoldFitterModel = ScaffoldFitterModel(self._context)
 
     def get_context(self):
@@ -32,20 +31,23 @@ class MasterModel(object):
     def get_align_scale(self):
         return self._scaffoldFitterModel.get_align_scale()
 
+    def get_initial_scale(self):
+        self._scaffoldFitterModel.get_initial_scale()
+
     def get_align_offset(self):
         return self._scaffoldFitterModel.get_align_offset()
 
     def get_align_euler_angles(self):
         return self._scaffoldFitterModel.get_align_euler_angles()
 
-    def initialise(self, point_cloud, region_initialised=True):
-        if region_initialised:
-            self._scaffoldFitterModel.initialise(point_cloud)
-        else:
-            self._scaffoldFitterModel.initialise(point_cloud, region_initialised=False)
+    def initialise(self, point_cloud, scaffold_path):
+        self._scaffoldFitterModel.initialise(point_cloud, scaffold_path)
 
-    def initialise_region(self, region):
-        self._scaffoldFitterModel.initialise_region(region)
+    def swap_yz(self):
+        self._scaffoldFitterModel.swap_axes(axes='yz')
+
+    def auto_align_model_on_data(self):
+        self._scaffoldFitterModel.auto_align_model_on_data()
 
     def is_align_mirror(self):
         self._scaffoldFitterModel.is_align_mirror()
